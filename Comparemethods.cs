689e8faf62acc1757edb879a919dea14603c5432
ChandraAwesome <120436175+ChandraAwesome@users.noreply.github.com>
2023-01-11 21:08:03 +0530

    Initial Commit

---
+++ Comparemethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    public static class Comparemethods
+    {
+        public static void Comparemethodss() 
+        {
+            Random random = new Random();
+            //
+            int x1 = random.Next(0, 10);
+            int y1 = random.Next(0, 10);
+            int x2 = random.Next(0, 10);
+            int y2 = random.Next(0, 10);
+            //
+            double LENGTH = Math.Sqrt(((x2 - x1) ^ 2) + ((y2 - y1) ^ 2));
+            double LENGTH1 = Math.Sqrt(((x2 - x1) ^ 2) + ((y2 - y1) ^ 2));
+            Console.WriteLine("length of line :" + LENGTH);
+            Console.WriteLine("length1 of line :" + LENGTH1);
+            Console.WriteLine("Comparison of lines :"+LENGTH.CompareTo(LENGTH1));
+        }
+    }
+}
---
+++ Lenthofline.cs
@@ -0,0 +1,19 @@
+﻿namespace LineComparison
+{
+    public class Lenthofline
+    {
+       public static void Main(string[] args)
+        {
+            //
+            Random random= new Random();
+            //
+            int x1 = random.Next(0,10);
+            int y1 = random.Next(0,10);
+            int x2 = random.Next(0,10);
+            int y2 = random.Next(0,10);
+            //
+            double LENGTH = Math.Sqrt(((x2-x1)^2)+((y2-y1)^2));
+            Console.WriteLine("length of line :"+LENGTH);
+        }
+    }
+}
